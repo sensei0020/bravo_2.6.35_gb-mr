57af42a7613d4d3d7bf08c76a2e4dbdd6b604117
worstenbrood <worstenbrood@gmail.com>
2012-09-07 21:57:37 +0200

    Added row i/o scheduler

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1217,6 +1217,7 @@ static inline void put_dev_sector(Sector p)
 
 struct work_struct;
 int kblockd_schedule_work(struct request_queue *q, struct work_struct *work);
+int kblockd_schedule_delayed_work(struct request_queue *q, struct delayed_work *work, unsigned long delay);
 
 #ifdef CONFIG_BLK_CGROUP
 /*
